b70c3c4e13951bfd806c8a749855767c9b59a646
souheibriache <gs_riache@esi.dz>
2023-10-29 01:37:17 +0200

    test: writing unit tests for the functions auth and justify

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use an official Node.js runtime as the base image
+FROM node:14
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the working directory
+COPY package*.json ./
+
+# Install project dependencies
+RUN npm install
+
+# Copy the rest of the project files to the working directory
+COPY . .
+
+# Expose a port if your project requires it
+# EXPOSE 3000
+
+# Specify the command to run your project
+CMD [ "npm", "start" ]
--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -1,203 +1,75 @@
-import { mock } from 'jest-mock-extended';
-import { Request, Response } from 'express';
-import fs from 'fs';
 import jwt from 'jsonwebtoken';
+import { Response } from 'express';
+import authenticateToken from '../app/middleware/auth';
+import { mock } from 'jest-mock-extended';
 import getToken from '../app/controllers/token';
 
-describe('getToken', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
+describe('authenticateToken middleware', () => {
+  it('should call next() if token is valid', () => {
+    const req:any = {
+      headers: {
+        authorization: 'Bearer validToken',
+      },
+    };
+
+    const res = mock<Response>();
+    const next = jest.fn();
+
+    jwt.verify = jest.fn().mockImplementation((token, secret, callback) => {
+      callback(null, 'test@example.com');
+    });
+
+    authenticateToken(req, res, next);
+
+    expect(next).toHaveBeenCalled();
+    expect(req.email).toBe('test@example.com');
+  });
+
+  it('should return 401 Unauthorized if token is missing', () => {
+    const req = {
+      headers: {},
+    };
+
+    const res = mock<Response>();
+    const next = jest.fn();
+
+    authenticateToken(req, res, next);
+
+    expect(res.sendStatus).toHaveBeenCalledWith(401);
+  });
+
+  it('should return 403 Forbidden if token is invalid', () => {
+    const req = {
+      headers: {
+        authorization: 'Bearer invalidToken',
+      },
+    };
+
+    const res = mock<Response>();
+    const next = jest.fn();
+
+    jwt.verify = jest.fn().mockImplementation((token, secret, callback) => {
+      callback(new Error('Invalid token'));
+    });
+
+    authenticateToken(req, res, next);
+
+    expect(res.sendStatus).toHaveBeenCalledWith(403);
   });
 
   it('should return 400 Bad Request if email is missing', () => {
-    const req = {
+    const req :any = {
       body: {},
     } as Request;
 
     const res = mock<Response>({
       status: jest.fn().mockReturnThis(),
       send: jest.fn(),
-    });
+    })  as unknown as Response;
 
     getToken(req, res);
 
     expect(res.status).toHaveBeenCalledWith(400);
     expect(res.send).toHaveBeenCalledWith('An email is required');
   });
-
-  it('should create the wordCounts.json file if it does not exist', () => {
-    jest.spyOn(fs, 'readFileSync').mockImplementation(() => {
-      throw { code: 'ENOENT' };
-    });
-
-    const writeFileSyncMock = jest.spyOn(fs, 'writeFileSync');
-
-    const req = {
-      body: {
-        userInput: {
-          email: 'test@example.com',
-        },
-      },
-    } as Request;
-
-    const res = mock<Response>();
-
-    getToken(req, res);
-
-    expect(writeFileSyncMock).toHaveBeenCalledWith(
-      'wordCounts.json',
-      JSON.stringify({})
-    );
-  });
-
-  it('should load existing user data from wordCounts.json', () => {
-    const existingUserData = {
-      'test@example.com': {
-        count: 100,
-        timestamp: null,
-      },
-    };
-
-    jest.spyOn(fs, 'readFileSync').mockReturnValue(
-      JSON.stringify(existingUserData)
-    );
-
-    const req = {
-      body: {
-        userInput: {
-          email: 'test@example.com',
-        },
-      },
-    } as Request;
-
-    const res = mock<Response>();
-
-    getToken(req, res);
-
-    expect(res.json).toHaveBeenCalledWith({ token: expect.any(String) });
-  });
-
-  it('should not overwrite existing user data if email already exists', () => {
-    const existingUserData = {
-      'test@example.com': {
-        count: 100,
-        timestamp: null,
-      },
-    };
-
-    jest.spyOn(fs, 'readFileSync').mockReturnValue(
-      JSON.stringify(existingUserData)
-    );
-
-    const writeFileSyncMock = jest.spyOn(fs, 'writeFileSync');
-
-    const req = {
-      body: {
-        userInput: {
-          email: 'test@example.com',
-        },
-      },
-    } as Request;
-
-    const res = mock<Response>();
-
-    getToken(req, res);
-
-    expect(writeFileSyncMock).not.toHaveBeenCalled();
-  });
-
-  it('should add email to user data if it does not exist', () => {
-    const existingUserData = {
-      'another@example.com': {
-        count: 200,
-        timestamp: null,
-      },
-    };
-
-    jest.spyOn(fs, 'readFileSync').mockReturnValue(
-      JSON.stringify(existingUserData)
-    );
-
-    const writeFileSyncMock = jest.spyOn(fs, 'writeFileSync');
-
-    const req = {
-      body: {
-        userInput: {
-          email: 'test@example.com',
-        },
-      },
-    } as Request;
-
-    const res = mock<Response>();
-
-    getToken(req, res);
-
-    expect(writeFileSyncMock).toHaveBeenCalledWith(
-      'wordCounts.json',
-      JSON.stringify({
-        ...existingUserData,
-        'test@example.com': {
-          count: 0,
-          timestamp: null,
-        },
-      })
-    );
-  });
-
-  it('should log an error if fs.writeFileSync throws an error', () => {
-    jest.spyOn(fs, 'readFileSync').mockReturnValue('{}');
-    jest.spyOn(fs, 'writeFileSync').mockImplementation(() => {
-      throw new Error('Write error');
-    });
-
-    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation();
-
-    const req = {
-      body: {
-        userInput: {
-          email: 'test@example.com',
-        },
-      },
-    } as Request;
-
-    const res = mock<Response>();
-
-    getToken(req, res);
-
-    expect(consoleErrorSpy).toHaveBeenCalledWith(
-      'Error writing users.json:',
-      expect.any(Error)
-    );
-  });
-
-  it('should generate a token and send it in the response', () => {
-    const existingUserData = {
-      'test@example.com': {
-        count: 100,
-        timestamp: null,
-      },
-    };
-
-    jest.spyOn(fs, 'readFileSync').mockReturnValue(
-      JSON.stringify(existingUserData)
-    );
-
-    const signMock = jest.spyOn(jwt, 'sign').mockReturnValue();
-
-    const req = {
-      body: {
-        userInput: {
-          email: 'test@example.com',
-        },
-      },
-    } as Request;
-
-    const res = mock<Response>({
-      json: jest.fn(),
-    });
-
-    getToken(req, res);
-
-    expect(res.json).toHaveBeenCalledWith({ token: 'test-token' });
-  })
-})
+});
